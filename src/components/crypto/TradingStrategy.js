b356598cf24b4d658680aeb3fcbbecab1fa23eb6
BogdanDarlau <darlau.bogdan@yahoo.com>
2023-08-29 16:19:04 +0300

    work test netlify

--- src/components/crypto/TradingStrategy.js
+++ src/components/crypto/TradingStrategy.js
@@ -9,6 +9,60 @@ const TradingStrategy = ({ cryptoList }) => {
   const { usdtBalance, setUsdtBalance } = useUsdtBalance();
   const [selectedCrypto, setSelectedCrypto] = useState(null);
   const [popupOpen, setPopupOpen] = useState(false);
+  const [cryptoSavedData, setCryptoSavedData] = useState([]);
+  const [allCryptoData, setAllCryptoData] = useState({});
+
+  // Carica i dati da Gist all'avvio dell'app
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const gistResponse = await axios.get(
+          "https://gist.github.com/BogdanDarlau/fa20eee5fd7085085611bacf285126b3"
+        );
+
+        // Estrai il contenuto del file data.json dal Gist
+        const dataJson = gistResponse.data.files["data.json"].content;
+        const parsedData = JSON.parse(dataJson);
+        console.log(parsedData);
+        setCryptoSavedData(parsedData);
+        setAllCryptoData(parsedData);
+      } catch (error) {
+        console.error("Error fetching data from Gist:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // Aggiungi questa parte sotto gli altri import e hook
+  const updateGistData = async (updatedData) => {
+    const gistUrl =
+      "https://gist.github.com/BogdanDarlau/fa20eee5fd7085085611bacf285126b3"; // Sostituisci con l'URL del tuo Gist
+    const accessToken = "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"; // Sostituisci con il tuo token di accesso
+
+    try {
+      const response = await axios.patch(
+        gistUrl,
+        {
+          files: {
+            "data.json": {
+              content: JSON.stringify(updatedData, null, 2), // 2 per indentare i dati
+            },
+          },
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+
+      return response.data;
+    } catch (error) {
+      console.error("Error updating Gist data:", error);
+      return null;
+    }
+  };
 
   const fetchCryptoPrices = async () => {
     try {
@@ -37,42 +91,34 @@ const TradingStrategy = ({ cryptoList }) => {
   }, []);
 
   useEffect(() => {
-    if (Object.keys(cryptoPrices).length > 0) {
+    if (
+      Object.keys(cryptoPrices).length > 0 &&
+      Object.keys(cryptoSavedData).length > 0
+    ) {
       const updatedStrategyData = {};
+      const allCryptoData = { ...cryptoSavedData }; // Copia i dati dei cryptoSavedData
 
       cryptoList.forEach((symbol) => {
         const currentPrice = cryptoPrices[symbol];
+        const cryptoSymbolData = allCryptoData[symbol];
         const currentStrategyData = strategyDataList[symbol] || {
           min: 0.95,
           max: 1.05,
           plusMin: 0.985,
           plusMax: 1.015,
           usdtIn: 5,
-          lastOrder: 750000,
-          totCoin: 0,
-          reference: 0,
-          stopLoss: 0,
-          takeProfit: 0,
-          start: true,
-          firstPurchase: false,
-          purchase: false,
-          sellMinus: false,
-          buyPlus: false,
-          sale: false,
-          saleWon: false,
-          saleLost: false,
-          lastOperation: "",
         };
 
         if (
-          currentStrategyData.start &&
+          cryptoSymbolData &&
+          cryptoSymbolData.start &&
           currentPrice <=
-            currentStrategyData.lastOrder * currentStrategyData.min &&
+            cryptoSymbolData.lastOrder * currentStrategyData.min &&
           usdtBalance >= currentStrategyData.usdtIn &&
           currentPrice !== 0
         ) {
           const updatedReference =
-            currentStrategyData.reference + currentStrategyData.usdtIn;
+            cryptoSymbolData.reference + currentStrategyData.usdtIn;
           const updatedTotCoin = (
             (updatedReference / currentPrice) *
             0.999
@@ -81,7 +127,7 @@ const TradingStrategy = ({ cryptoList }) => {
 
           // Esempio di modifica dello stato
           updatedStrategyData[symbol] = {
-            ...currentStrategyData,
+            ...cryptoSymbolData,
             lastOrder: currentPrice,
             reference: updatedReference,
             totCoin: updatedTotCoin,
@@ -94,9 +140,26 @@ const TradingStrategy = ({ cryptoList }) => {
         }
       });
 
+      // Aggiorna i dati nel Gist solo se sono cambiati
+      if (JSON.stringify(allCryptoData) !== JSON.stringify(cryptoSavedData)) {
+        updateGistData(allCryptoData).then((updatedData) => {
+          if (updatedData) {
+            console.log("Gist data updated successfully:", updatedData);
+            setCryptoSavedData(allCryptoData); // Aggiorna anche lo stato locale
+          }
+        });
+      }
+
       setStrategyDataList(updatedStrategyData);
     }
-  }, [cryptoPrices, cryptoList, strategyDataList, usdtBalance, setUsdtBalance]);
+  }, [
+    cryptoPrices,
+    cryptoList,
+    strategyDataList,
+    usdtBalance,
+    setUsdtBalance,
+    cryptoSavedData,
+  ]);
 
   const handleOpenPopup = (symbol) => {
     setSelectedCrypto(symbol);
@@ -121,12 +184,10 @@ const TradingStrategy = ({ cryptoList }) => {
       {popupOpen && selectedCrypto && (
         <div className="popup">
           <p className="crypto-name">{selectedCrypto}</p>
-          <p>Last Order: {strategyDataList[selectedCrypto]?.lastOrder}</p>
-          <p>Reference: {strategyDataList[selectedCrypto]?.reference}</p>
-          <p>Total Coin: {strategyDataList[selectedCrypto]?.totCoin}</p>
-          <p>
-            Last Operation: {strategyDataList[selectedCrypto]?.lastOperation}{" "}
-          </p>
+          <p>Last Order: {allCryptoData[selectedCrypto]?.lastOrder}</p>
+          <p>Reference: {allCryptoData[selectedCrypto]?.reference}</p>
+          <p>Total Coin: {allCryptoData[selectedCrypto]?.totCoin}</p>
+          <p>Last Operation: {allCryptoData[selectedCrypto]?.lastOperation}</p>
           <div className="btn">
             <button onClick={handleClosePopup}>Close</button>
           </div>
