5560988560b31092eeca2e0e0874811efddbceff
BogdanDarlau <darlau.bogdan@yahoo.com>
2023-09-02 15:39:31 +0300

    8th commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import Crypto from "./components/crypto/Crypto";
 import TradingStrategy from "./components/crypto/TradingStrategy";
 import cryptoList from "./components/crypto/crypto.json";
 import { UsdtBalanceProvider } from "./components/crypto/usdtBalance";
@@ -12,9 +11,6 @@ const App = () => {
         <div className="page">
           <div className="crypto-name-list">
             <TradingStrategy cryptoList={cryptoList} />
-          </div>
-          <div className="crypto-prices">
-            <Crypto cryptoList={cryptoList} />
           </div>
         </div>
       </UsdtBalanceProvider>
